f1de040d74b58dd14483877ced1e87d600d18dcc
Marcel Dombrowski <marcel.dombrowski@unb.ca>
2017-04-21 14:27:51 -0300

    - started on the song engine

---
+++ Assets/Scripts/SongEngine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KoMa
+{
+	public class SongEngine : MonoBehaviour
+	{
+		// Use this for initialization
+		void Start ()
+		{
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+	}
+}
