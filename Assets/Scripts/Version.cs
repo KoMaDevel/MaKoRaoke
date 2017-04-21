fd5acdbdd5aea3d14b2b1b997e2a9aa53ced831e
Marcel Dombrowski <marcel.dombrowski@unb.ca>
2017-04-21 14:08:32 -0300

    Set up Unity project
    - Set up Unity project
    - Created main menu scene and sample code

---
+++ Assets/Scripts/Version.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KoMa
+{
+	public class Version : MonoBehaviour
+	{
+		public string AppTitle = "MaKoRaoke";
+
+		void Awake ()
+		{
+			gameObject.GetComponent<Text> ().text = AppTitle + " v" + Application.version;
+		}
+	}
+}
